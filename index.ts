53852057e18702d7b8f0634e4279ef9604ca2be2
mery <meryscarlet15@gmail.com>
2021-03-26 18:33:54 +0100

    Add custom port

--- index.ts
+++ index.ts
@@ -1,15 +1,4 @@
 import { createServer } from "./src/config/server/server";
 import { createDBConnection } from "./src/config/db/db";
 
-createDBConnection()
-  .then(() => {
-    console.log("Mongodb is running");
-    try {
-      createServer(4000);
-    } catch (error) {
-      console.log("Error running the server");
-    }
-  })
-  .catch((error) => {
-    console.log("Error Mongo connect");
-  });
+createServer(process.env.PORT || 4000);
