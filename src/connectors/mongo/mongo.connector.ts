9c1f280c010c480ca33542f30c9da453d672d243
mery <meryscarlet15@gmail.com>
2021-03-26 19:08:58 +0100

    fix mongo-conector

--- src/connectors/mongo/mongo.connector.ts
+++ src/connectors/mongo/mongo.connector.ts
@@ -3,7 +3,8 @@ import { MONGO_CONFIG } from "../../constants/data/config-mongo";
 
 const mongoAuth =
   MONGO_CONFIG.user && MONGO_CONFIG.userPassword ? `${MONGO_CONFIG.user}:${MONGO_CONFIG.userPassword}` : "";
-const COMPLETE_URL = `${MONGO_CONFIG.type}://${mongoAuth}${MONGO_CONFIG.host}/${MONGO_CONFIG.database}${MONGO_CONFIG.params}`;
+const mongoParams = MONGO_CONFIG.params ? `?${MONGO_CONFIG.params}` : "";
+const COMPLETE_URL = `${MONGO_CONFIG.type}://${mongoAuth}${MONGO_CONFIG.host}/${MONGO_CONFIG.database}${mongoParams}`;
 
 export const initDBConnection = () => {
   mongoose.Promise = global.Promise;
